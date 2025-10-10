c22ab406172ebc7d0f0251fecef1ec66f3649174
AyanKhan98 <ayankhann98@gmail.com>
2025-10-10 22:30:53 +0530

    Initial Commit

---
+++ Guess The Place/Guess_The_PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_PlaceApp.swift
+//  Guess The Place
+//
+//  Created by Ayan khan on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
